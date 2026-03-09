767124f3a204a5377ac6d4ebcfb31d2bd39ac032
Funda Aker <fundaker@outlook.com>
2026-03-10 01:37:52 +0300

    Initial Commit

---
+++ WeeklyPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeeklyPlanner
+//
+//  Created by Funda Aker on 10.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeeklyPlanner/WeeklyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyPlannerApp.swift
+//  WeeklyPlanner
+//
+//  Created by Funda Aker on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
